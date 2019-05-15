a89814c70cfcc95ebfeff93247a5e7628cc35336
Joshua Emmons <skia@skia.net>
2019-05-14 20:11:41 -0500

    Adds path component matchers.
    Passing an array to `Path` or `PathPrefix` initializers will match based on the given compnents rather than the path string.

--- SessionArtist/URL+PathMatcher.swift
+++ SessionArtist/URL+PathMatcher.swift
@@ -4,43 +4,73 @@ import Foundation
 
 public extension URL {
   struct Host {
-    public let host: String
+    private let host: String
+    
+    
     public init(_ host: String) {
       self.host = host
+    }
+    
+    
+    public static func ~= (pattern: URL.Host, value: URL) -> Bool {
+      return pattern.host == value.host
     }
   }
   
   
   struct Path {
-    public let path: String
+    private let pathRepresentation: PathRepresentation
+
+
     public init(_ path: String) {
-      self.path = path
+      pathRepresentation = .string(path)
+    }
+    
+    
+    public init(_ components: [String]) {
+      pathRepresentation = .components(components)
+    }
+
+
+    public static func ~= (pattern: URL.Path, value: URL) -> Bool {
+      switch pattern.pathRepresentation {
+      case .string(let path):
+        return path == value.path
+      case .components(let components):
+        return components == value.pathComponents
+      }
     }
   }
   
   
   struct PathPrefix {
-    public let pathPrefix: String
+    private let pathPrefixRepresentation: PathRepresentation
+    
+    
     public init(_ pathPrefix: String) {
-      self.pathPrefix = pathPrefix
+      pathPrefixRepresentation = .string(pathPrefix)
+    }
+    
+    
+    public init(_ components: [String]) {
+      pathPrefixRepresentation = .components(components)
+    }
+   
+    
+    public static func ~= (pattern: URL.PathPrefix, value: URL) -> Bool {
+      switch pattern.pathPrefixRepresentation {
+      case .string(let path):
+        return value.path.hasPrefix(path)
+      case .components(let components):
+        let valueComponentsPrefix = Array(value.pathComponents.prefix(components.count))
+        return valueComponentsPrefix == components
+      }
     }
   }
 }
 
 
-
-public func ~= (pattern: URL.Host, value: URL) -> Bool {
-  return pattern.host == value.host
-}
-
-
-
-public func ~= (pattern: URL.Path, value: URL) -> Bool {
-  return pattern.path == value.path
-}
-
-
-
-public func ~= (pattern: URL.PathPrefix, value: URL) -> Bool {
-  return value.path.hasPrefix(pattern.pathPrefix)
+private enum PathRepresentation {
+  case string(String)
+  case components([String])
 }
--- SessionArtistTests/URLPatternMatcherTests.swift
+++ SessionArtistTests/URLPatternMatcherTests.swift
@@ -32,7 +32,52 @@ class URLPatternMatcherTests: XCTestCase {
       XCTFail()
     }
   }
+
   
+  func testMatchesComponents() {
+    switch URL(string: "http://example.com/foo/bar")! {
+    case URL.Path(["foo", "bar"]):
+      XCTFail()
+    case URL.Path(["/", "foo", "barr", "baz"]):
+      XCTFail()
+    case URL.Path(["/", "foo", "bar", "baz"]):
+      XCTFail()
+    case URL.Path(["/", "foo", "ba"]):
+      XCTFail()
+    case URL.Path(["/", "foo"]):
+      XCTFail()
+    case URL.Path(["/"]):
+      XCTFail()
+    case URL.Path(["/", "foo", "bar"]):
+      XCTAssert(true)
+    default:
+      XCTFail()
+    }
+
+    guard case URL.Path(["/", "foo", "bar"]) = URL(string: "http://example.com/foo/bar/")! else {
+      XCTFail()
+      return
+    }
+
+    guard case URL.Path([]) = URL(string: "http://example.com")! else {
+      XCTFail()
+      return
+    }
+
+    if case URL.Path(["/"]) = URL(string: "http://example.com")! {
+      XCTFail()
+    }
+
+    switch URL(string: "http://example.com/")! {
+    case URL.Path([]):
+      XCTFail()
+    case URL.Path(["/"]):
+      XCTAssert(true)
+    default:
+      XCTFail()
+    }
+  }
+
   
   func testMatchesPathPrefix() {
     let subject = URL(string: "http://example.com/foo/bar")!
@@ -60,6 +105,83 @@ class URLPatternMatcherTests: XCTestCase {
     }
 
     if case URL.PathPrefix("/foo/bo") = subject {
+      XCTFail()
+    }
+  }
+  
+  
+  func testMatchesComponentsPrefix() {
+    let foobar = URL(string: "http://example.com/foo/bar")!
+    guard case URL.PathPrefix([]) = foobar else {
+      XCTFail()
+      return
+    }
+
+    guard case URL.PathPrefix(["/"]) = foobar else {
+      XCTFail()
+      return
+    }
+
+    guard case URL.PathPrefix(["/", "foo"]) = foobar else {
+      XCTFail()
+      return
+    }
+    
+    guard case URL.PathPrefix(["/", "foo", "bar"]) = foobar else {
+      XCTFail()
+      return
+    }
+    
+    if case URL.PathPrefix(["/", "foo", "barr"]) = foobar {
+      XCTFail()
+    }
+
+    if case URL.PathPrefix(["/", "foo", "bar", "baz"]) = foobar {
+      XCTFail()
+    }
+    
+    if case URL.PathPrefix(["/", "foo", "ba"]) = foobar {
+      XCTFail()
+    }
+
+    if case URL.PathPrefix(["foo", "bar"]) = foobar {
+      XCTFail()
+    }
+
+    
+    switch URL(string: "http://example.com/foo/bar/")! {
+    case URL.PathPrefix(["/", "foo", "bar", "baz"]):
+      XCTFail()
+    
+    case URL.PathPrefix(["/", "foo", "barr"]):
+      XCTFail()
+    
+    case URL.PathPrefix(["/", "foo", "ba"]):
+      XCTFail()
+    
+    case URL.PathPrefix(["/", "foo", "bar", "/"]):
+      XCTFail()
+    
+    case URL.PathPrefix(["/", "foo", "bar"]):
+      XCTAssert(true)
+    
+    default:
+      XCTFail()
+    }
+    
+    guard case URL.PathPrefix(["/"]) = URL(string: "http://example.com/")! else {
+      XCTFail()
+      return
+    }
+    
+    switch URL(string: "http://example.com")! {
+    case URL.PathPrefix(["/"]):
+      XCTFail()
+    
+    case URL.PathPrefix([]):
+      XCTAssert(true)
+    
+    default:
       XCTFail()
     }
   }
