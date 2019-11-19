98723c44c96d48ceda592f2f412239ca2665517e
Joshua Emmons <skia@skia.net>
2019-11-19 15:44:36 -0600

    Debugging remote test failures.

--- Tests/SessionArtistTests/DeleteTests.swift
+++ Tests/SessionArtistTests/DeleteTests.swift
@@ -11,40 +11,43 @@ class DeleteTests: XCTestCase {
   
   
   func testDelete() {
-    doFakeDelete("/test", headers: [:]) { req in
-      XCTAssert(true, "endpoint exists")
+    let expectedRequest = expectation(description: "waiting for request")
+    let expectedResponse = expectation(description: "waiting for response")
+
+    FakeServer.runWith { server in
+      server.add(Route(method: .delete, path: "/test")) { req in
+        XCTAssert(true, "endpoint exists")
+        expectedRequest.fulfill()
+      }
+      
+      fakeHost.delete("/test").data { res in
+        if case .success(.ok, _, _) = res {
+          expectedResponse.fulfill()
+        }
+      }
+
+      wait(for: [expectedRequest, expectedResponse], timeout: 1)
     }
   }
   
   
   func testDeleteHeaders() {
-    doFakeDelete("/test", headers: [.accept: "foobar"]) { req in
-      XCTAssertEqual(req.allHTTPHeaderFields!["Accept"], "foobar")
-    }
-  }
-}
-
-
-
-private extension DeleteTests {
-  func doFakeDelete(_ path: String, headers: [HTTPHeaderField: String], putHandler: @escaping (URLRequest)->Void) {
     let expectedRequest = expectation(description: "waiting for request")
     let expectedResponse = expectation(description: "waiting for response")
-    
+
     FakeServer.runWith { server in
-      server.add(Route(method: .delete, path: path)) { req in
-        putHandler(req)
+      server.add(Route(method: .delete, path: "/test")) { req in
+        XCTAssertEqual(req.allHTTPHeaderFields!["Accept"], "foobar")
         expectedRequest.fulfill()
       }
       
-      fakeHost.delete(path, headers: headers).data { res in
+      fakeHost.delete("/test", headers: [.accept: "foobar"]).data { res in
         if case .success(.ok, _, _) = res {
           expectedResponse.fulfill()
         }
       }
-      
+
       wait(for: [expectedRequest, expectedResponse], timeout: 1)
     }
   }
 }
-
